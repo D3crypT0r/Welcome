ed7b9984053ec2305278ee58c3235ac7b5ca8d9d
(Syed Rizwan Hilal Shah) <66831571+D3crypT0r@users.noreply.github.com>
2024-11-21 16:02:07 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,16 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect('users.db')
+conn = sqlite3.connect('yourdb.db')
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM users")
+cursor.execute("SELECT * FROM "yourtable")
 rows = cursor.fetchall()
 
 print("Registration Data:")
 for row in rows:
     print(row)
 
-cursor.execute("SELECT * FROM users")
+cursor.execute("SELECT * FROM "yourtable")
 rows = cursor.fetchall()
 
 print("\nLogin Data:")
